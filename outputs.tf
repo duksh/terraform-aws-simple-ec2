5a39e89106ee569722c896481d8e65bfef2993d9
Duksh Koonjoobeeharry <arkadux14@gmail.com>
2024-01-11 12:15:04 +0400

    refactor: :bug: Changed output block ami to sg-id
    Changed the output block 'ami' to 'sg-id' so that it makes sense when reading the code
    
    None

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,11 @@
-output "ami" {
+#This block will output the Security Group ID
+output "sg-id" {
   value       = aws_security_group.dk-res-sg-1.id
   description = "ID of the security group(s) that have been created"
 }
 
-output "created_instance" {
+# This block will output the EC2 instance ID
+output "ec2-id" {
   value       = aws_instance.dk-res-ec2-1.id
-  description = "ID of the created instance(s)"
+  description = "ID of the EC2 instance(s) that have been created"
 }
