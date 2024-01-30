31a109e5b5581533f626a4c3edff8ce65920661e
Duksh Koonjoobeeharry <arkadux14@gmail.com>
2024-01-30 11:04:13 +0400

    fix: :bug: Updated the outputs.tf file for count errors
    Updated the outputs.tf file for count related errors
    
    None

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,12 @@
 #This block will output the Security Group ID
 output "sg-id" {
-  value       = aws_security_group.dk-res-sg-1.id
+  value       = aws_security_group.dk-res-sg-1.*.id
   description = "ID of the security group(s) that have been created"
 }
 
+
 # This block will output the EC2 instance ID
 output "ec2-id" {
-  value       = aws_instance.dk-res-ec2-1.id
+  value       = aws_instance.dk-ec2.*.id
   description = "ID of the EC2 instance(s) that have been created"
 }
